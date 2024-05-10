05533a9c57853213549ec1086fe327ea0e69612e
RocketBoy86 <76099297+RocketBoy86@users.noreply.github.com>
2024-05-10 18:38:59 +1000

    Debug Dockerfile

---
+++ app/imaginarium/page.tsx
@@ -0,0 +1,11 @@
+import { MantineProvider, ColorSchemeScript, Blockquote, Container } from '@mantine/core';
+
+export default function Imaginarium() {
+  return (
+    <>
+    <Container>
+      <Blockquote color="blue" cite="– Timothy Richardson" mt="xl">A page awaiting content is a blank page.</Blockquote>
+    </Container>
+    </>
+  );
+}
