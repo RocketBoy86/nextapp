2c9e3487f45974fe980920ea255165e9293d85ad
RocketBoy86 <76099297+RocketBoy86@users.noreply.github.com>
2024-05-11 11:20:46 +1000

    Add card components
    Develop card components and card gallery.

--- app/imaginarium/page.tsx
+++ app/imaginarium/page.tsx
@@ -1,10 +1,22 @@
-import { MantineProvider, ColorSchemeScript, Blockquote, Container } from '@mantine/core';
+import { MantineProvider, ColorSchemeScript, Blockquote, Container, Flex, Space } from '@mantine/core';
+
+import { IconCodeCircle2, IconGlobe, IconAtom2, IconAffiliate, IconSignRight, IconCpu, IconAward, IconPuzzle } from '@tabler/icons-react';
+
+
+import { ColorSchemeToggle } from '../../components/ColorSchemeToggle/ColorSchemeToggle';
+
+import BoatCard from '@/app/imaginarium/CardGallery';
+import CardGallery from '@/app/imaginarium/CardGallery';
+
+
 
 export default function Imaginarium() {
   return (
     <>
     <Container>
       <Blockquote color="blue" cite="– Timothy Richardson" mt="xl">A page awaiting content is a blank page.</Blockquote>
+      <Space h="xl"></Space>
+      <CardGallery></CardGallery>
     </Container>
     </>
   );
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,12 @@
 'use client'
 
-import { AppShell, Burger, Group, Skeleton, Space, Image } from '@mantine/core';
+import { AppShell, Burger, Group, Space, Image, Flex, MantineProvider } from '@mantine/core';
 import { useDisclosure } from '@mantine/hooks';
-import { MantineLogo } from '@mantinex/mantine-logo';
 import '@mantine/core/styles.css';
 import React from 'react';
-import { MantineProvider, ColorSchemeScript } from '@mantine/core';
 import { theme } from '../theme';
 import { ColorSchemeToggle } from '../components/ColorSchemeToggle/ColorSchemeToggle';
 import NavBar from '@/components/Navbar';
-import myImage from '../app/rd_logo1.jpg';
 
 export default function RootLayout({
   children,
@@ -21,31 +18,43 @@ export default function RootLayout({
 
   return (
     <html lang="en">
-      <body><MantineProvider defaultColorScheme='dark'>
-    <AppShell
-      header={{ height: 60 }}
-      footer={{ height: 60 }}
-      navbar={{ width: 300, breakpoint: 'sm', collapsed: { mobile: !opened } }}
-      aside={{ width: 300, breakpoint: 'md', collapsed: { desktop: false, mobile: true } }}
-      padding="md"
-    >
-      <AppShell.Header>
-        <Group h="100%" px="md">
-          <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
-          <Image radius="md" h={50} w={50} fit="contain" src={myImage} />
-        </Group>
-      </AppShell.Header>
-      <AppShell.Navbar p="md" onClick={toggle}>
-        <NavBar />
-      </AppShell.Navbar>
-      <AppShell.Main>
-        <Space h="xl"/>
-        {children}
-      </AppShell.Main>
-      <AppShell.Aside p="md">Aside.. what to do wioth this or even keep it at all?</AppShell.Aside>
-      <AppShell.Footer>< ColorSchemeToggle /></AppShell.Footer>
-    </AppShell>
-    </MantineProvider></body>
+      <head>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+        <link rel="icon" type="image/png" sizes="32x32" href="/app/favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+        <link rel="manifest" href="/site.webmanifest"/>
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5"/>
+        <meta name="msapplication-TileColor" content="#da532c"/>
+        <meta name="theme-color" content="#ffffff"/>
+      </head>
+      <body>
+        <MantineProvider defaultColorScheme='dark'>
+          <AppShell
+          header={{ height: 60 }}
+          footer={{ height: 60 }}
+          navbar={{ width: 300, breakpoint: 'sm', collapsed: { mobile: !opened } }}
+          padding="md">
+            <AppShell.Header>
+              <Group h="100%" px="md">
+                <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
+                <Image radius="lg" h={50} w={50} fit="contain" src="/rd_logo1.jpg" />
+              </Group>
+            </AppShell.Header>
+            <AppShell.Navbar p="md" onClick={toggle}>
+              <NavBar />
+            </AppShell.Navbar>
+            <AppShell.Main>
+              <Space h="xl"/>
+              {children}
+            </AppShell.Main>
+            <AppShell.Footer>
+              <Flex mih={50} justify="left" align="flex-start" direction="row">
+                <ColorSchemeToggle/>
+              </Flex>
+              </AppShell.Footer>
+          </AppShell>
+        </MantineProvider>
+      </body>
     </html>
   )
 }
